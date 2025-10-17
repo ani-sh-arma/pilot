aa85e66f8e51f9f3217dff52dc8e99018f83159a
Anish <anisharma030@gmail.com>
2025-10-18 00:02:04 +0530

    Increased file size limit

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -16,7 +16,7 @@ export const ourFileRouter = {
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
-      maxFileSize: "32MB",
+      maxFileSize: "1GB",
       maxFileCount: 10,
     },
   })
